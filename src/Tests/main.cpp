f1fd533579f7b7af438bdc53afb7184f228ae94a
Yury <gbalog@inbox.ru>
2020-04-17 12:17:50 +0500

    Logging enabled in the test application

--- src/Tests/main.cpp
+++ src/Tests/main.cpp
@@ -1,7 +1,36 @@
 #include <gtest/gtest.h>
+#include "TracerConfig.h"
+#include "ConsoleWriter.h"
+#include "FileWriter.h"
+#include "Tracer.h"
+
+static
+  bool initLogging()
+{
+  FileWriterParams fileWrParams;
+  fileWrParams._filePattern = "socks5_test.log";
+  fileWrParams._maxBytes = 1024 * 1024 * 10; //10 MByte
+  fileWrParams._maxFiles = 10;
+
+  try
+  {
+    auto con = std::make_shared<ConsoleWriter>();
+    TracerConfig::instance().addWriter(con);
+
+    auto file = std::make_shared<FileWriter>(fileWrParams);
+    TracerConfig::instance().addWriter(file);
+  }
+  catch (const std::runtime_error & ex)
+  {
+    TRACE_FORCE(ERR, "Init") << ex.what();
+    return false;
+  }
+  return true;
+}
 
 int main(int argc, char * argv[])
 {
+  initLogging();
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
